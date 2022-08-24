8c9e3dd0db2917496cfe84778b71e35a461f6dce
aWZHY0yQH81uOYvH <32046476+aWZHY0yQH81uOYvH@users.noreply.github.com>
2022-08-24 13:48:53 -0700

    CUDA: fixed memory access
    now seems to run but has no extensions

--- FDTD/engine_cuda.cu
+++ FDTD/engine_cuda.cu
@@ -21,6 +21,8 @@ Engine_CUDA::Engine_CUDA(const Operator_CUDA* op) : Engine::Engine(op)
 	m_supports_coop_launch = 0;
 	m_gridDim = {0};
 	m_blockDim = {0};
+	Op = op;
+	m_type = CUDA;
 }
 
 void Engine_CUDA::setCUDAdevice(unsigned int cuda_device_number) {
@@ -88,21 +90,21 @@ __device__ void UpdateVoltages(CUDA_VECTOR ***volt, CUDA_VECTOR ***curr, CUDA_VE
 	volt[x][y][z] = v;
 }
 
-__device__ void UpdateCurrents(Engine_CUDA *instance, unsigned int x, unsigned int y, unsigned int z)
+__device__ void UpdateCurrents(CUDA_VECTOR ***volt, CUDA_VECTOR ***curr, CUDA_VECTOR ***opiv, CUDA_VECTOR ***opii, unsigned int x, unsigned int y, unsigned int z)
 {
-	CUDA_VECTOR i = instance->curr[x][y][z];
-	CUDA_VECTOR v = instance->volt[x][y][z];
-	CUDA_VECTOR vx = instance->volt[x+1][y][z];
-	CUDA_VECTOR vy = instance->volt[x][y+1][z];
-	CUDA_VECTOR vz = instance->volt[z][y][z+1];
-	CUDA_VECTOR iv = instance->Op->iv[x][y][z];
-	CUDA_VECTOR ii = instance->Op->ii[x][y][z];
+	CUDA_VECTOR i = curr[x][y][z];
+	CUDA_VECTOR v = volt[x][y][z];
+	CUDA_VECTOR vx = volt[x+1][y][z];
+	CUDA_VECTOR vy = volt[x][y+1][z];
+	CUDA_VECTOR vz = volt[z][y][z+1];
+	CUDA_VECTOR iv = opiv[x][y][z];
+	CUDA_VECTOR ii = opii[x][y][z];
 
 	i.x = i.x * ii.x + iv.x * (v.z - vy.z - v.y + vz.y);
 	i.y = i.y * ii.y + iv.y * (v.x - vz.x - v.z + vx.z);
 	i.z = i.z * ii.z + iv.z * (v.y - vx.y - v.x + vy.x);
 
-	instance->curr[x][y][z] = i;
+	curr[x][y][z] = i;
 }
 
 /*__global__ void ManyTS(Engine_CUDA *instance, unsigned int startTS, unsigned int iterTS, unsigned int numLines[])
@@ -140,28 +142,28 @@ __device__ void UpdateCurrents(Engine_CUDA *instance, unsigned int x, unsigned i
 	}
 }*/
 
-__global__ void VoltageKernel(CUDA_VECTOR ***volt, CUDA_VECTOR ***curr, CUDA_VECTOR ***opvi, CUDA_VECTOR ***opvv, unsigned int numLines[]) {
+__global__ void VoltageKernel(CUDA_VECTOR ***volt, CUDA_VECTOR ***curr, CUDA_VECTOR ***opvi, CUDA_VECTOR ***opvv, unsigned int numLinesX, unsigned int numLinesY, unsigned int numLinesZ) {
 	const unsigned int pos[] = {
 		blockDim.x * blockIdx.x + threadIdx.x,
 		blockDim.y * blockIdx.y + threadIdx.y,
 		blockDim.z * blockIdx.z + threadIdx.z
 	};
 
-	if(pos[0] >= numLines[0] || pos[1] >= numLines[1] || pos[2] >= numLines[2]) return;
+	if(pos[0] >= numLinesX || pos[1] >= numLinesY || pos[2] >= numLinesZ) return;
 
 	UpdateVoltages(volt, curr, opvi, opvv, pos[0], pos[1], pos[2]);
 }
 
-__global__ void CurrentKernel(Engine_CUDA *instance, unsigned int numLines[]) {
+__global__ void CurrentKernel(CUDA_VECTOR ***volt, CUDA_VECTOR ***curr, CUDA_VECTOR ***opvi, CUDA_VECTOR ***opvv, unsigned int numLinesX, unsigned int numLinesY, unsigned int numLinesZ) {
 	const unsigned int pos[] = {
 		blockDim.x * blockIdx.x + threadIdx.x,
 		blockDim.y * blockIdx.y + threadIdx.y,
 		blockDim.z * blockIdx.z + threadIdx.z
 	};
 
-	if(pos[0] >= numLines[0]-1 || pos[1] >= numLines[1]-1 || pos[2] >= numLines[2]-1) return;
+	if(pos[0] >= numLinesX-1 || pos[1] >= numLinesY-1 || pos[2] >= numLinesZ-1) return;
 
-	UpdateCurrents(instance, pos[0], pos[1], pos[2]);
+	UpdateCurrents(volt, curr, opvi, opvv, pos[0], pos[1], pos[2]);
 }
 
 bool Engine_CUDA::IterateTS(unsigned int iterTS)
@@ -182,13 +184,14 @@ bool Engine_CUDA::IterateTS(unsigned int iterTS)
 	unsigned int endTS = numTS + iterTS;
 	for(; numTS < endTS; numTS++)
 	{
-		VoltageKernel<<<m_gridDim, m_blockDim>>>(volt, curr, Op->vi, Op->vv, numLines);
+		VoltageKernel<<<m_gridDim, m_blockDim>>>(volt, curr, Op->vi, Op->vv, numLines[0], numLines[1], numLines[2]);
 		cudaError_t err = cudaGetLastError();
 		if(err) throw std::runtime_error("CUDA kernel launch failure: " + std::string(cudaGetErrorString(err)));
-		//CurrentKernel<<<m_gridDim, m_blockDim>>>(this, numLines);
-		//err = cudaGetLastError();
-		//if(err) throw std::runtime_error("CUDA kernel launch failure: " + std::string(cudaGetErrorString(err)));
+		CurrentKernel<<<m_gridDim, m_blockDim>>>(volt, curr, Op->iv, Op->ii, numLines[0], numLines[1], numLines[2]);
+		err = cudaGetLastError();
+		if(err) throw std::runtime_error("CUDA kernel launch failure: " + std::string(cudaGetErrorString(err)));
 	}
+	cudaDeviceSynchronize();
 	return true;
 }
 
